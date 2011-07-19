b2fb921be0d169c324fb651286f659e090840b49
Miguel Pastor <miguel.pastor@liferay.com>
2011-07-19 11:51:50 +0200

    LPS-18742 SQL migration process to convert the current data related with user groups and site templates to the new data model

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_1_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_1_0.java
@@ -34,6 +34,7 @@ import com.liferay.portal.upgrade.v6_1_0.UpgradeScheduler;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeSchema;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeScopes;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeSubscription;
+import com.liferay.portal.upgrade.v6_1_0.UpgradeUserGroup;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeUserName;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeVirtualHost;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeWorkflow;
@@ -69,6 +70,7 @@ public class UpgradeProcess_6_1_0 extends UpgradeProcess {
 		upgrade(UpgradeScheduler.class);
 		upgrade(UpgradeScopes.class);
 		upgrade(UpgradeSubscription.class);
+		upgrade(UpgradeUserGroup.class);
 		upgrade(UpgradeUserName.class);
 		upgrade(UpgradeVirtualHost.class);
 		upgrade(UpgradeWorkflow.class);
