2304be6ab662b43a23008b6be2a824a4e1f063b7
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2012-07-10 15:41:17 +0200

    LPS-28465

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppHelperLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppHelperLocalServiceImpl.java
@@ -385,6 +385,11 @@ public class DLAppHelperLocalServiceImpl
 			userId, fileVersion.getFileVersionId(), fileVersion.getStatus(),
 			new HashMap<String, Serializable>(), serviceContext);
 
+		// File shortcut
+
+		dlFileShortcutLocalService.enableFileShortcuts(
+			fileEntry.getFileEntryId());
+
 		// File rank
 
 		dlFileRankLocalService.enableFileRanks(fileEntry.getFileEntryId());
@@ -971,6 +976,17 @@ public class DLAppHelperLocalServiceImpl
 						WorkflowConstants.STATUS_IN_TRASH)) {
 
 					continue;
+				}
+
+				// File shortcut
+
+				if (status == WorkflowConstants.STATUS_APPROVED) {
+					dlFileShortcutLocalService.enableFileShortcuts(
+						dlFileEntry.getFileEntryId());
+				}
+				else {
+					dlFileShortcutLocalService.disableFileShortcuts(
+						dlFileEntry.getFileEntryId());
 				}
 
 				// Asset
