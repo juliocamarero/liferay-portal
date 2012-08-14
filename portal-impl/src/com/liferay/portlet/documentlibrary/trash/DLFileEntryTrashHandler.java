431335366154bee397e0347cdb31abe1407f6781
Zsolt Berentey <zsolt.berentey@liferay.com>
2012-08-14 18:12:43 +0200

    LPS-29170 Apply new move from trash to document library

--- portal-impl/src/com/liferay/portlet/documentlibrary/trash/DLFileEntryTrashHandler.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/trash/DLFileEntryTrashHandler.java
@@ -14,16 +14,15 @@
 
 package com.liferay.portlet.documentlibrary.trash;
 
-import com.liferay.portal.InvalidRepositoryException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.repository.Repository;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.trash.BaseTrashHandler;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.repository.liferayrepository.LiferayRepository;
 import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.service.RepositoryServiceUtil;
+import com.liferay.portal.service.ServiceContext;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFileVersion;
 import com.liferay.portlet.documentlibrary.model.DLFolder;
@@ -47,7 +46,7 @@ import javax.portlet.PortletRequest;
  * @author Manuel de la Peña
  * @author Zsolt Berentey
  */
-public class DLFileEntryTrashHandler extends BaseTrashHandler {
+public class DLFileEntryTrashHandler extends DLBaseTrashHandler {
 
 	public static final String CLASS_NAME = DLFileEntry.class.getName();
 
@@ -154,6 +153,24 @@ public class DLFileEntryTrashHandler extends BaseTrashHandler {
 		return DLUtil.getAbsolutePath(portletRequest, dlFolder.getFolderId());
 	}
 
+	@Override
+	public boolean isRestorable(long classPK)
+		throws PortalException, SystemException {
+
+		DLFileEntry dlFileEntry = getDLFileEntry(classPK);
+
+		return !dlFileEntry.isInTrashFolder();
+	}
+
+	@Override
+	public void moveTrashEntry(
+			long classPK, long containerId, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		DLAppServiceUtil.moveFileEntryFromTrash(
+			classPK, containerId, serviceContext);
+	}
+
 	/**
 	 * Restores all file entries with the matching primary keys.
 	 *
@@ -184,23 +201,6 @@ public class DLFileEntryTrashHandler extends BaseTrashHandler {
 		dlFileVersion.setTitle(name);
 
 		DLFileVersionLocalServiceUtil.updateDLFileVersion(dlFileVersion, false);
-	}
-
-	protected DLFileEntry getDLFileEntry(long classPK)
-		throws PortalException, SystemException {
-
-		Repository repository = RepositoryServiceUtil.getRepositoryImpl(
-			0, classPK, 0);
-
-		if (!(repository instanceof LiferayRepository)) {
-			throw new InvalidRepositoryException(
-				"Repository " + repository.getRepositoryId() +
-					" does not support trash operations");
-		}
-
-		FileEntry fileEntry = repository.getFileEntry(classPK);
-
-		return (DLFileEntry)fileEntry.getModel();
 	}
 
 }
