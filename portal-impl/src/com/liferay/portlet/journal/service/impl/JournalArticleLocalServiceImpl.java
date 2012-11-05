39c5b0c88542976ea59950eace952d801740c2cf
Juan Fernández <juan.fernandez@liferay.com>
2012-11-05 09:30:11 +0100

    LPS-30744 Apply new constant

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -316,7 +316,7 @@ public class JournalArticleLocalServiceImpl
 
 		// Workflow
 
-		if (classNameId == 0) {
+		if (classNameId == JournalArticleConstants.CLASSNAME_ID_DEFAULT) {
 			WorkflowHandlerRegistryUtil.startWorkflowInstance(
 				user.getCompanyId(), groupId, userId,
 				JournalArticle.class.getName(), article.getId(), article,
@@ -358,8 +358,9 @@ public class JournalArticleLocalServiceImpl
 		int displayDateMinute = calendar.get(Calendar.MINUTE);
 
 		return addArticle(
-			userId, groupId, folderId, 0, 0, StringPool.BLANK, true, 1,
-			titleMap, descriptionMap, content, "general", structureId,
+			userId, groupId, folderId,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, 0, StringPool.BLANK,
+			true, 1, titleMap, descriptionMap, content, "general", structureId,
 			templateId, null, displayDateMonth, displayDateDay, displayDateYear,
 			displayDateHour, displayDateMinute, 0, 0, 0, 0, 0, true, 0, 0, 0, 0,
 			0, true, true, false, null, null, null, null, serviceContext);
@@ -434,7 +435,8 @@ public class JournalArticleLocalServiceImpl
 
 		List<JournalArticle> articles =
 			journalArticleFinder.findByExpirationDate(
-				0, WorkflowConstants.STATUS_APPROVED,
+				JournalArticleConstants.CLASSNAME_ID_DEFAULT,
+				WorkflowConstants.STATUS_APPROVED,
 				new Date(now.getTime() + _JOURNAL_ARTICLE_CHECK_INTERVAL));
 
 		if (_log.isDebugEnabled()) {
@@ -479,7 +481,8 @@ public class JournalArticleLocalServiceImpl
 		}
 
 		articles = journalArticleFinder.findByReviewDate(
-			0, now, new Date(now.getTime() - _JOURNAL_ARTICLE_CHECK_INTERVAL));
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, now,
+			new Date(now.getTime() - _JOURNAL_ARTICLE_CHECK_INTERVAL));
 
 		if (_log.isDebugEnabled()) {
 			_log.debug(
@@ -2394,7 +2397,9 @@ public class JournalArticleLocalServiceImpl
 
 		boolean visible = article.isApproved();
 
-		if (article.getClassNameId() > 0) {
+		if (article.getClassNameId() !=
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT) {
+
 			visible = false;
 		}
 
@@ -2557,7 +2562,8 @@ public class JournalArticleLocalServiceImpl
 					}
 				}
 
-				if (article.getClassNameId() == 0) {
+				if (article.getClassNameId() ==
+					JournalArticleConstants.CLASSNAME_ID_DEFAULT) {
 
 					// Get the earliest display date and latest expiration date
 					// among all article versions
@@ -2591,7 +2597,8 @@ public class JournalArticleLocalServiceImpl
 			}
 		}
 
-		if (article.getClassNameId() == 0) {
+		if (article.getClassNameId() ==
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT) {
 
 			// Email
 
@@ -3529,7 +3536,7 @@ public class JournalArticleLocalServiceImpl
 			throw le;
 		}
 
-		if ((classNameId == 0) &&
+		if ((classNameId == JournalArticleConstants.CLASSNAME_ID_DEFAULT) &&
 			(titleMap.isEmpty() ||
 			 Validator.isNull(titleMap.get(articleDefaultLocale)))) {
 
@@ -3568,7 +3575,9 @@ public class JournalArticleLocalServiceImpl
 					throw new NoSuchTemplateException();
 				}
 			}
-			else if (classNameId == 0) {
+			else if (classNameId ==
+				JournalArticleConstants.CLASSNAME_ID_DEFAULT) {
+
 				throw new NoSuchTemplateException();
 			}
 		}
