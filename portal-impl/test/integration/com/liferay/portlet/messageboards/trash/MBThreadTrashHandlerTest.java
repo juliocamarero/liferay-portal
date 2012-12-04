dfa2d2eafd927ca730feedc07b08bb439393520e
Eduardo Garcia <eduardo.garcia@liferay.com>
2012-12-04 23:18:56 +0100

    LPS-29174 Integration tests for Trash Handlers in Message Boards

--- portal-impl/test/integration/com/liferay/portlet/messageboards/trash/MBThreadTrashHandlerTest.java
+++ portal-impl/test/integration/com/liferay/portlet/messageboards/trash/MBThreadTrashHandlerTest.java
@@ -12,37 +12,37 @@
  * details.
  */
 
-package com.liferay.portlet.trash;
+package com.liferay.portlet.messageboards.trash;
 
 import com.liferay.portal.kernel.dao.orm.QueryDefinition;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
-import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.BaseModel;
 import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.model.Group;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceTestUtil;
 import com.liferay.portal.test.EnvironmentExecutionTestListener;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.TransactionalExecutionTestListener;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.messageboards.model.MBCategory;
 import com.liferay.portlet.messageboards.model.MBCategoryConstants;
 import com.liferay.portlet.messageboards.model.MBMessage;
-import com.liferay.portlet.messageboards.model.MBMessageConstants;
 import com.liferay.portlet.messageboards.model.MBThread;
+import com.liferay.portlet.messageboards.service.MBCategoryServiceUtil;
 import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
-import com.liferay.portlet.messageboards.service.MBMessageServiceUtil;
 import com.liferay.portlet.messageboards.service.MBThreadLocalServiceUtil;
 import com.liferay.portlet.messageboards.service.MBThreadServiceUtil;
-
-import java.io.InputStream;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.liferay.portlet.trash.BaseTrashHandlerTestCase;
 
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 
 /**
  * @author Zsolt Berentey
+ * @author Eduardo Garcia
  */
 @ExecutionTestListeners(
 	listeners = {
@@ -53,17 +53,17 @@ import org.junit.runner.RunWith;
 public class MBThreadTrashHandlerTest extends BaseTrashHandlerTestCase {
 
 	@Override
+	public void testTrashAndDeleteDraft() throws Exception {
+		Assert.assertTrue("This test does not apply", true);
+	}
+
+	@Override
+	public void testTrashAndRestoreDraft() throws Exception {
+		Assert.assertTrue("This test does not apply", true);
+	}
+
+	@Override
 	public void testTrashDuplicate() throws Exception {
-		Assert.assertTrue("This test does not apply", true);
-	}
-
-	@Override
-	public void testTrashParentAndDeleteParent() throws Exception {
-		Assert.assertTrue("This test does not apply", true);
-	}
-
-	@Override
-	public void testTrashParentAndRestoreModel() throws Exception {
 		Assert.assertTrue("This test does not apply", true);
 	}
 
@@ -83,20 +83,11 @@ public class MBThreadTrashHandlerTest extends BaseTrashHandlerTestCase {
 			ServiceContext serviceContext)
 		throws Exception {
 
-		String subject = _SUBJECT;
-		String body = _BODY;
-		String format = MBMessageConstants.DEFAULT_FORMAT;
-		boolean anonymous = false;
-		double priority = 0.0;
-		boolean allowPingbacks = false;
-		List<ObjectValuePair<String, InputStream>> inputStreamOVPs =
-			new ArrayList<ObjectValuePair<String, InputStream>>(5);
+		MBCategory category = (MBCategory)parentBaseModel;
 
-		MBMessage message = MBMessageServiceUtil.addMessage(
-			serviceContext.getScopeGroupId(),
-			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID, subject, body,
-			format, inputStreamOVPs, anonymous, priority, allowPingbacks,
-			serviceContext);
+		MBMessage message = MBMessageLocalServiceUtil.addMessage(
+			TestPropsValues.getUserId(), ServiceTestUtil.randomString(),
+			category.getCategoryId(), _SUBJECT, _BODY, serviceContext);
 
 		if (!approved) {
 			message = MBMessageLocalServiceUtil.updateStatus(
@@ -124,8 +115,26 @@ public class MBThreadTrashHandlerTest extends BaseTrashHandlerTestCase {
 		QueryDefinition queryDefinition = new QueryDefinition(
 			WorkflowConstants.STATUS_ANY);
 
+		MBCategory category = (MBCategory)parentBaseModel;
+
 		return MBThreadLocalServiceUtil.getGroupThreadsCount(
-			(Long)parentBaseModel.getPrimaryKeyObj(), queryDefinition);
+			category.getGroupId(), queryDefinition);
+	}
+
+	@Override
+	protected BaseModel<?> getParentBaseModel(
+			Group group, ServiceContext serviceContext)
+		throws Exception {
+
+		return MBCategoryServiceUtil.addCategory(
+			TestPropsValues.getUserId(),
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID,
+			ServiceTestUtil.randomString(), StringPool.BLANK, serviceContext);
+	}
+
+	@Override
+	protected Class<?> getParentBaseModelClass() {
+		return MBCategory.class;
 	}
 
 	@Override
@@ -144,13 +153,47 @@ public class MBThreadTrashHandlerTest extends BaseTrashHandlerTestCase {
 	}
 
 	@Override
-	protected boolean isIndexableBaseModel() {
-		return false;
+	protected boolean isInTrashFolder(ClassedModel classedModel)
+		throws Exception {
+
+		MBThread thread = (MBThread)classedModel;
+
+		return thread.isInTrashCategory();
+	}
+
+	@Override
+	protected BaseModel<?> moveBaseModelFromTrash(
+			ClassedModel classedModel, Group group,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		BaseModel<?> parentBaseModel = getParentBaseModel(
+			group, serviceContext);
+
+		MBThreadServiceUtil.moveThreadFromTrash(
+			(Long)parentBaseModel.getPrimaryKeyObj(),
+			(Long)classedModel.getPrimaryKeyObj());
+
+		return parentBaseModel;
 	}
 
 	@Override
 	protected void moveBaseModelToTrash(long primaryKey) throws Exception {
 		MBThreadServiceUtil.moveThreadToTrash(primaryKey);
+	}
+
+	@Override
+	protected void moveParentBaseModelToTrash(long primaryKey)
+		throws Exception {
+
+		MBCategoryServiceUtil.moveCategoryToTrash(primaryKey);
+	}
+
+	@Override
+	protected int searchBaseModelsCount(Class<?> clazz, long groupId)
+		throws Exception {
+
+		return super.searchBaseModelsCount(MBMessage.class, groupId);
 	}
 
 	private static final String _BODY = "Body";
