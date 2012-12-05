b807b2e2f9ffd702dcb97aa71b82d639c40cd1b8
ealonso <eudaldo.alonso@liferay.com>
2012-12-05 09:06:35 +0100

    LPS-31390 Missing breadcrumb in Recycle Bin

--- portal-impl/src/com/liferay/portlet/wiki/trash/WikiPageTrashHandler.java
+++ portal-impl/src/com/liferay/portlet/wiki/trash/WikiPageTrashHandler.java
@@ -31,12 +31,16 @@ import com.liferay.portlet.trash.TrashEntryConstants;
 import com.liferay.portlet.trash.model.TrashEntry;
 import com.liferay.portlet.trash.util.TrashUtil;
 import com.liferay.portlet.wiki.asset.WikiPageAssetRenderer;
+import com.liferay.portlet.wiki.model.WikiNode;
 import com.liferay.portlet.wiki.model.WikiPage;
 import com.liferay.portlet.wiki.model.WikiPageResource;
 import com.liferay.portlet.wiki.service.WikiPageLocalServiceUtil;
 import com.liferay.portlet.wiki.service.WikiPageResourceLocalServiceUtil;
 import com.liferay.portlet.wiki.service.WikiPageServiceUtil;
 import com.liferay.portlet.wiki.service.permission.WikiPagePermission;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
@@ -109,6 +113,14 @@ public class WikiPageTrashHandler extends BaseTrashHandler {
 		return CLASS_NAME;
 	}
 
+	public String getContainerModelClassName() {
+		return WikiNode.class.getName();
+	}
+
+	public String getContainerModelName() {
+		return "wiki-node";
+	}
+
 	@Override
 	public ContainerModel getParentContainerModel(long classPK)
 		throws PortalException, SystemException {
@@ -116,6 +128,17 @@ public class WikiPageTrashHandler extends BaseTrashHandler {
 		WikiPage page = WikiPageLocalServiceUtil.getPage(classPK);
 
 		return page.getNode();
+	}
+
+	@Override
+	public List<ContainerModel> getParentContainerModels(long containerModelId)
+		throws PortalException, SystemException {
+
+		List<ContainerModel> containerModels = new ArrayList<ContainerModel>();
+
+		containerModels.add(getParentContainerModel(containerModelId));
+
+		return containerModels;
 	}
 
 	@Override
