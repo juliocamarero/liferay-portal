7682ef70478309ab4834bea6e398dfc3da57cf67
Juan Fernández <juan.fernandez@liferay.com>
2013-01-08 13:36:53 +0100

    LPS-29501 Javadocs source formatting

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -1011,11 +1011,12 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 	}
 
 	/**
-	 * Returns all the sites that are children of the parent group.
+	 * Returns all the groups that are children of the parent group.
 	 *
 	 * @param  companyId the primary key of the company
-	 * @param  parentGroupId the primary key of the parent group
-	 * @param  site whether the group is to be associated with a main site
+	 * @param  parentGroupId the primary key of the parent group.
+	 *         this can be set to -1 to be ignored
+	 * @param  site whether the returned groups are sites
 	 * @return the matching groups, or <code>null</code> if no matches were
 	 *         found
 	 * @throws SystemException if a system exception occurred
