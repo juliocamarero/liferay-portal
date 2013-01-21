f0f98a3bafb30a66e4e71de7264d31b7d79d3b16
Mate Thurzo <mate.thurzo@liferay.com>
2013-01-21 13:47:01 +0100

    LPS-27530 Applying changes to BookmarksPortletDataHandlerImpl according to the new environment

--- portal-impl/src/com/liferay/portlet/bookmarks/lar/BookmarksPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/bookmarks/lar/BookmarksPortletDataHandlerImpl.java
@@ -14,27 +14,24 @@
 
 package com.liferay.portlet.bookmarks.lar;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.kernel.lar.PortletDataHandlerControl;
-import com.liferay.portal.kernel.util.MapUtil;
-import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.lar.StagedModelDataHandler;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerRegistryUtil;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
-import com.liferay.portal.service.ServiceContext;
-import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.bookmarks.model.BookmarksEntry;
 import com.liferay.portlet.bookmarks.model.BookmarksFolder;
 import com.liferay.portlet.bookmarks.model.BookmarksFolderConstants;
 import com.liferay.portlet.bookmarks.service.BookmarksEntryLocalServiceUtil;
 import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
-import com.liferay.portlet.bookmarks.service.persistence.BookmarksEntryUtil;
 import com.liferay.portlet.bookmarks.service.persistence.BookmarksFolderUtil;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.portlet.PortletPreferences;
 
@@ -43,6 +40,8 @@ import javax.portlet.PortletPreferences;
  * @author Bruno Farache
  * @author Raymond Augé
  * @author Juan Fernández
+ * @author Mate Thurzo
+ * @author Daniel Kocsis
  */
 public class BookmarksPortletDataHandlerImpl extends BasePortletDataHandler {
 
@@ -131,17 +130,41 @@ public class BookmarksPortletDataHandlerImpl extends BasePortletDataHandler {
 		List<BookmarksFolder> folders = BookmarksFolderUtil.findByGroupId(
 			portletDataContext.getScopeGroupId());
 
+		StagedModelDataHandler<BookmarksFolder> bookmarksFolderDataHandler =
+			(StagedModelDataHandler<BookmarksFolder>)
+				StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
+					BookmarksFolder.class.getName());
+
 		for (BookmarksFolder folder : folders) {
-			exportFolder(
-				portletDataContext, foldersElement, entriesElement, folder);
+			if (!portletDataContext.isWithinDateRange(
+					folder.getModifiedDate())) {
+
+				continue;
+			}
+
+			bookmarksFolderDataHandler.exportModelData(
+				folder, portletDataContext, foldersElement, entriesElement);
 		}
 
-		List<BookmarksEntry> entries = BookmarksEntryUtil.findByG_F(
-			portletDataContext.getScopeGroupId(),
-			BookmarksFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+		List<BookmarksEntry> entries =
+			BookmarksEntryLocalServiceUtil.getGroupEntries(
+				portletDataContext.getScopeGroupId(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+
+		StagedModelDataHandler<BookmarksEntry> bookmarksEntryDataHandler =
+			(StagedModelDataHandler<BookmarksEntry>)
+				StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
+					BookmarksEntry.class.getName());
 
 		for (BookmarksEntry entry : entries) {
-			exportEntry(portletDataContext, null, entriesElement, entry);
+			if (!portletDataContext.isWithinDateRange(
+					entry.getModifiedDate())) {
+
+				continue;
+			}
+
+			bookmarksEntryDataHandler.exportModelData(
+				entry, portletDataContext, foldersElement, entriesElement);
 		}
 
 		return document.formattedString();
@@ -164,32 +187,26 @@ public class BookmarksPortletDataHandlerImpl extends BasePortletDataHandler {
 
 		Element foldersElement = rootElement.element("folders");
 
+		StagedModelDataHandler<BookmarksFolder> bookmarksFolderDataHandler =
+			(StagedModelDataHandler<BookmarksFolder>)
+				StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
+					BookmarksFolder.class.getName());
+
 		for (Element folderElement : foldersElement.elements("folder")) {
-			String path = folderElement.attributeValue("path");
-
-			if (!portletDataContext.isPathNotProcessed(path)) {
-				continue;
-			}
-
-			BookmarksFolder folder =
-				(BookmarksFolder)portletDataContext.getZipEntryAsObject(path);
-
-			importFolder(portletDataContext, path, folder);
+			bookmarksFolderDataHandler.importModelData(
+				folderElement, portletDataContext);
 		}
 
 		Element entriesElement = rootElement.element("entries");
 
+		StagedModelDataHandler<BookmarksEntry> bookmarksEntryDataHandler =
+			(StagedModelDataHandler<BookmarksEntry>)
+				StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
+					BookmarksEntry.class.getName());
+
 		for (Element entryElement : entriesElement.elements("entry")) {
-			String path = entryElement.attributeValue("path");
-
-			if (!portletDataContext.isPathNotProcessed(path)) {
-				continue;
-			}
-
-			BookmarksEntry entry =
-				(BookmarksEntry)portletDataContext.getZipEntryAsObject(path);
-
-			importEntry(portletDataContext, entryElement, entry);
+			bookmarksEntryDataHandler.importModelData(
+				entryElement, portletDataContext);
 		}
 
 		return null;
