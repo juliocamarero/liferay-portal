2a3df1ccb039f73e40c601857e5abe4c97295ba8
KocsisDaniel <daniel.kocsis@liferay.com>
2013-02-26 16:32:40 +0100

    LPS-33083 Autogenerated

--- sql/portal-tables.sql
+++ sql/portal-tables.sql
@@ -1232,8 +1232,13 @@ create table MBThread (
 );
 
 create table MBThreadFlag (
+	uuid_ VARCHAR(75) null,
 	threadFlagId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
 	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
 	modifiedDate DATE null,
 	threadId LONG
 );
