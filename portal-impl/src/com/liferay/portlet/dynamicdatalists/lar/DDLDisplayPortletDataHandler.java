728a64c7e3b07bc94e5600f40a3b443529553d9c
Mate Thurzo <mate.thurzo@liferay.com>
2013-03-11 15:05:18 +0100

    LPS-33626 Apply same pattern to all portlet data handlers

--- portal-impl/src/com/liferay/portlet/dynamicdatalists/lar/DDLDisplayPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatalists/lar/DDLDisplayPortletDataHandler.java
@@ -22,9 +22,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
-import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portlet.dynamicdatalists.model.DDLRecordSet;
 import com.liferay.portlet.dynamicdatalists.service.DDLRecordSetLocalServiceUtil;
 import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
@@ -63,7 +61,7 @@ public class DDLDisplayPortletDataHandler extends DDLPortletDataHandler {
 	}
 
 	@Override
-	protected String doExportData(
+	protected void doExportData(
 			PortletDataContext portletDataContext, String portletId,
 			PortletPreferences portletPreferences)
 		throws Exception {
@@ -80,10 +78,10 @@ public class DDLDisplayPortletDataHandler extends DDLPortletDataHandler {
 				_log.debug("No record set found for " + portletId);
 			}
 
-			return StringPool.BLANK;
+			return;
 		}
 
-		Element rootElement = addExportRootElement();
+		Element rootElement = portletDataContext.getRootElement();
 
 		DDLRecordSet recordSet = DDLRecordSetLocalServiceUtil.getRecordSet(
 			recordSetId);
@@ -91,13 +89,13 @@ public class DDLDisplayPortletDataHandler extends DDLPortletDataHandler {
 		StagedModelDataHandlerUtil.exportStagedModel(
 			portletDataContext, rootElement, recordSet);
 
-		return rootElement.formattedString();
+		return;
 	}
 
 	@Override
 	protected PortletPreferences doImportData(
 			PortletDataContext portletDataContext, String portletId,
-			PortletPreferences portletPreferences, String data)
+			PortletPreferences portletPreferences)
 		throws Exception {
 
 		portletDataContext.importPermissions(
@@ -105,13 +103,7 @@ public class DDLDisplayPortletDataHandler extends DDLPortletDataHandler {
 			portletDataContext.getSourceGroupId(),
 			portletDataContext.getScopeGroupId());
 
-		if (Validator.isNull(data)) {
-			return null;
-		}
-
-		Document document = SAXReaderUtil.read(data);
-
-		Element rootElement = document.getRootElement();
+		Element rootElement = portletDataContext.getRootElement();
 
 		Element recordSetElement = rootElement.element("record-set");
 
@@ -152,6 +144,15 @@ public class DDLDisplayPortletDataHandler extends DDLPortletDataHandler {
 			"formDDMTemplateId", String.valueOf(formDDMTemplateId));
 
 		return portletPreferences;
+	}
+
+	@Override
+	protected boolean isValidData(String data) {
+		if (Validator.isNull(data)) {
+			return false;
+		}
+
+		return super.isValidData(data);
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(
