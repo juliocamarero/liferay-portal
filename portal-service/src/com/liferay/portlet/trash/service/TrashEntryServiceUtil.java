c316a075abfe8bcb5c20d6c4f3b2ec98f155ff4f
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-03-12 15:17:35 +0100

    LPS-30443 Generated

--- portal-service/src/com/liferay/portlet/trash/service/TrashEntryServiceUtil.java
+++ portal-service/src/com/liferay/portlet/trash/service/TrashEntryServiceUtil.java
@@ -70,6 +70,73 @@ public class TrashEntryServiceUtil {
 	}
 
 	/**
+	* Deletes the trash entries with the primary keys.
+	*
+	* @param entryIds the primary keys of the trash entries
+	* @throws PortalException if the user didn't have permission to delete one
+	or more entries
+	* @throws SystemException if a system exception occurred
+	*/
+	public static void deleteEntries(long[] entryIds)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteEntries(entryIds);
+	}
+
+	/**
+	* Deletes the trash entry with the primary key.
+	*
+	* <p>
+	* This method throws a PrincipalException if the user didn't have the
+	* permissions to perform the necessary operation. The exception is created
+	* with different messages for different operations:
+	* </p>
+	*
+	* <ul>
+	* <li>
+	* trash.delete.error - if the permission to delete the item was missing
+	* </li>
+	* </ul>
+	*
+	* @param entryId the primary key of the trash entry
+	* @throws PortalException if the user didn't have permission to delete the
+	entry
+	* @throws SystemException if a system exception occurred
+	*/
+	public static void deleteEntry(long entryId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteEntry(entryId);
+	}
+
+	/**
+	* Deletes the trash entry with the entity class name and primary key.
+	*
+	* <p>
+	* This method throws a PrincipalException if the user didn't have the
+	* permissions to perform the necessary operation. The exception is created
+	* with different messages for different operations:
+	* </p>
+	*
+	* <ul>
+	* <li>
+	* trash.delete.error - if the permission to delete the item was missing
+	* </li>
+	* </ul>
+	*
+	* @param className the class name of the entity
+	* @param classPK the primary key of the entity
+	* @throws PortalException if the user didn't have permission to delete the
+	entry
+	* @throws SystemException if a system exception occurred
+	*/
+	public static void deleteEntry(java.lang.String className, long classPK)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteEntry(className, classPK);
+	}
+
+	/**
 	* Returns the trash entries with the matching group ID.
 	*
 	* @param groupId the primary key of the group
@@ -104,6 +171,88 @@ public class TrashEntryServiceUtil {
 		throws com.liferay.portal.kernel.exception.SystemException,
 			com.liferay.portal.security.auth.PrincipalException {
 		return getService().getEntries(groupId, start, end, obc);
+	}
+
+	/**
+	* Restores the trash entry with the primary key by moving it to a new
+	* location identified by destination container model ID.
+	*
+	* <p>
+	* This method throws a PrincipalException if the user didn't have the
+	* permissions to perform one of the necessary operations. The exception is
+	* created with different messages for different operations:
+	* </p>
+	*
+	* <ul>
+	* <li>
+	* trash.move.error - if the permission to add the item to the new
+	* destination was missing
+	* </li>
+	* <li>
+	* trash.restore.error - if the permission to restore the item from trash
+	* was missing
+	* </li>
+	* </ul>
+	*
+	* @param groupId the primary key of the group
+	* @param entryId the primary key of the trash entry
+	* @param destinationContainerModelId the primary key of the new location
+	* @param serviceContext the service context (optionally <code>null</code>)
+	* @throws PortalException if the user didn't have permission to add the
+	entry to its new location or to restore it from the trash in
+	general
+	* @throws SystemException if a system exception occurred
+	*/
+	public static void moveEntry(java.lang.String className, long classPK,
+		long destinationContainerModelId,
+		com.liferay.portal.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getService()
+			.moveEntry(className, classPK, destinationContainerModelId,
+			serviceContext);
+	}
+
+	/**
+	* Restores to trash entry to its original location. In case of duplication,
+	* on of the optional parameters are set indicating either to overwrite the
+	* existing item or to rename the entry before restore.
+	*
+	* <p>
+	* This method throws a PrincipalException if the user didn't have the
+	* permissions to perform one of the necessary operations. The exception is
+	* created with different messages for different operations:
+	* </p>
+	*
+	* <ul>
+	* <li>
+	* trash.restore.error - if the permission to restore the item from trash
+	* was missing
+	* </li>
+	* <li>
+	* trash.restore.overwrite.error - if the permission to delete the existing
+	* item was missing
+	* </li>
+	* <li>
+	* trash.restore.rename.error - if the permission to rename the entry was
+	* missing
+	* </li>
+	* </ul>
+	*
+	* @param entryId the primary key of the trash entry
+	* @param overrideClassPK the primary key of the item to overwrite
+	* @param name the new name of the entry (optionally <code>null</code>)
+	* @return the trash entry that was restored
+	* @throws PortalException if the user didn't have permission to overwrite
+	an existing item, to rename the entry or to restore the entry
+	from the trash in general
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.liferay.portlet.trash.model.TrashEntry restoreEntry(
+		long entryId, long overrideClassPK, java.lang.String name)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService().restoreEntry(entryId, overrideClassPK, name);
 	}
 
 	public static TrashEntryService getService() {
