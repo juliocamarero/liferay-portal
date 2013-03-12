1303e2535d6fcf46a85c2c7d9bdffa5a510add3d
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-03-12 16:53:19 +0100

    LPS-30443 Remove permission checks from trash handler as well as multiple delete and restore methods, since we do not support them

--- portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryServiceImpl.java
@@ -310,11 +310,13 @@ public class TrashEntryServiceImpl extends TrashEntryServiceBaseImpl {
 
 		if (trashHandler.isInTrash(classPK)) {
 			trashHandler.moveTrashEntry(
-				classPK, destinationContainerModelId, serviceContext);
+				getUserId(), classPK, destinationContainerModelId,
+				serviceContext);
 		}
 		else {
 			trashHandler.moveEntry(
-				classPK, destinationContainerModelId, serviceContext);
+				getUserId(), classPK, destinationContainerModelId,
+				serviceContext);
 		}
 	}
 
