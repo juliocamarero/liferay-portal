4914ff1c28ba979260f145d4cea3866646b6dff9
Mate Thurzo <mate.thurzo@liferay.com>
2013-04-12 11:21:42 +0200

    LPS-32783 Avoiding possible NPEs

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
@@ -1687,13 +1687,13 @@ public class PortletDataContextImpl implements PortletDataContext {
 			parentStagedModel);
 
 		if (stagedModelElement == null) {
-			return null;
+			return Collections.emptyList();
 		}
 
 		Element referencesElement = stagedModelElement.element("references");
 
 		if (referencesElement == null) {
-			return null;
+			return Collections.emptyList();
 		}
 
 		XPath xPath = SAXReaderUtil.createXPath(
