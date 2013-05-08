398a7391375b200cbc97f2fa03aa46d13aba1d4d
KocsisDaniel <daniel.kocsis@liferay.com>
2013-05-08 04:40:13 +0200

    LPS-34705 Apply API changes

--- portal-impl/src/com/liferay/portal/lar/ExportImportImpl.java
+++ portal-impl/src/com/liferay/portal/lar/ExportImportImpl.java
@@ -313,7 +313,7 @@ public class ExportImportImpl implements ExportImport {
 					portletDataContext, fileEntry);
 
 				portletDataContext.addReferenceElement(
-					entityElement, fileEntry, FileEntry.class);
+					entityElement, fileEntry, FileEntry.class, true);
 
 				String path = ExportImportPathUtil.getModelPath(
 					fileEntry.getGroupId(), FileEntry.class.getName(),
