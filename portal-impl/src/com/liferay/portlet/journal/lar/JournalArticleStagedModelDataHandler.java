c9056bd5e2b3a65c719b77d651e2ae3af7473736
Julio Camarero <julio.camarero@liferay.com>
2013-05-15 14:28:26 -0700

    LPS-35351 rename methods (no logic changes)

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalArticleStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalArticleStagedModelDataHandler.java
@@ -159,7 +159,7 @@ public class JournalArticleStagedModelDataHandler
 
 			if (Validator.isNotNull(article.getSmallImageURL())) {
 				String smallImageURL =
-					ExportImportUtil.exportContentReferences(
+					ExportImportUtil.replaceExportContentReferences(
 						portletDataContext, article, articleElement,
 						article.getSmallImageURL().concat(StringPool.SPACE));
 
@@ -198,7 +198,7 @@ public class JournalArticleStagedModelDataHandler
 		if (portletDataContext.getBooleanParameter(
 				JournalPortletDataHandler.NAMESPACE, "embedded-assets")) {
 
-			String content = ExportImportUtil.exportContentReferences(
+			String content = ExportImportUtil.replaceExportContentReferences(
 				portletDataContext, article, articleElement,
 				article.getContent());
 
@@ -285,7 +285,7 @@ public class JournalArticleStagedModelDataHandler
 		Element articleElement =
 			portletDataContext.getImportDataStagedModelElement(article);
 
-		content = ExportImportUtil.importContentReferences(
+		content = ExportImportUtil.replaceImportContentReferences(
 			portletDataContext, articleElement, content);
 
 		article.setContent(content);
@@ -506,7 +506,7 @@ public class JournalArticleStagedModelDataHandler
 
 			if (Validator.isNotNull(article.getSmallImageURL())) {
 				String smallImageURL =
-					ExportImportUtil.importContentReferences(
+					ExportImportUtil.replaceImportContentReferences(
 						portletDataContext, articleElement,
 						article.getSmallImageURL());
 
