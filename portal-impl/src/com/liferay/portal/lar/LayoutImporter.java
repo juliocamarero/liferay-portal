7bc0a5ade6aef2a161cb2964e34652a741d1e6f9
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-06-14 11:32:34 +0200

    LPS-35834 Define setup as an option of portlet configuration
    To export/import setup, archived setups or user preferences, the configuration option must be enabled

--- portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
+++ portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
@@ -816,23 +816,23 @@ public class LayoutImporter {
 			Map<String, String[]> parameterMap, Element portletDataElement)
 		throws Exception {
 
+		boolean importPortletConfiguration = MapUtil.getBoolean(
+			parameterMap, PortletDataHandlerKeys.PORTLET_CONFIGURATION);
+		boolean importPortletConfigurationAll = MapUtil.getBoolean(
+			parameterMap, PortletDataHandlerKeys.PORTLET_CONFIGURATION_ALL);
 		boolean importPortletData = MapUtil.getBoolean(
 			parameterMap, PortletDataHandlerKeys.PORTLET_DATA);
 		boolean importPortletDataAll = MapUtil.getBoolean(
 			parameterMap, PortletDataHandlerKeys.PORTLET_DATA_ALL);
-		boolean importPortletSetup = MapUtil.getBoolean(
-			parameterMap, PortletDataHandlerKeys.PORTLET_SETUP);
-		boolean importPortletSetupAll = MapUtil.getBoolean(
-			parameterMap, PortletDataHandlerKeys.PORTLET_SETUP_ALL);
 
 		if (_log.isDebugEnabled()) {
 			_log.debug("Import portlet data " + importPortletData);
 			_log.debug("Import all portlet data " + importPortletDataAll);
-			_log.debug("Import portlet setup " + importPortletSetup);
+			_log.debug(
+				"Import portlet configuration " + importPortletConfiguration);
 		}
 
 		boolean importCurPortletData = importPortletData;
-		boolean importCurPortletSetup = importPortletSetup;
 
 		String rootPortletId = getRootPortletId(companyId, portletId);
 
@@ -851,19 +851,53 @@ public class LayoutImporter {
 						StringPool.UNDERLINE + rootPortletId);
 		}
 
-		if (importPortletSetupAll) {
-			importCurPortletSetup = true;
+		boolean importCurPortletArchivedSetups = importPortletConfiguration;
+		boolean importCurPortletSetup = importPortletConfiguration;
+		boolean importCurPortletUserPreferences = importPortletConfiguration;
+
+		if (importPortletConfigurationAll) {
+			importCurPortletArchivedSetups =
+				MapUtil.getBoolean(
+					parameterMap,
+					PortletDataHandlerKeys.PORTLET_ARCHIVED_SETUPS_ALL);
+			importCurPortletSetup =
+				MapUtil.getBoolean(
+					parameterMap, PortletDataHandlerKeys.PORTLET_SETUP_ALL);
+			importCurPortletUserPreferences =
+				MapUtil.getBoolean(
+					parameterMap,
+					PortletDataHandlerKeys.PORTLET_USER_PREFERENCES_ALL);
 		}
 		else if (rootPortletId != null) {
+			boolean importCurPortletConfiguration =
+				importPortletConfiguration &&
+				MapUtil.getBoolean(
+					parameterMap,
+					PortletDataHandlerKeys.PORTLET_CONFIGURATION +
+						StringPool.UNDERLINE + rootPortletId);
+			importCurPortletArchivedSetups =
+				importCurPortletConfiguration &&
+				MapUtil.getBoolean(
+					parameterMap,
+					PortletDataHandlerKeys.PORTLET_ARCHIVED_SETUPS +
+						StringPool.UNDERLINE + rootPortletId);
 			importCurPortletSetup =
-				importPortletSetup &&
+				importCurPortletConfiguration &&
 				MapUtil.getBoolean(
 					parameterMap,
 					PortletDataHandlerKeys.PORTLET_SETUP +
 						StringPool.UNDERLINE + rootPortletId);
+			importCurPortletUserPreferences =
+				importCurPortletConfiguration &&
+				MapUtil.getBoolean(
+					parameterMap,
+					PortletDataHandlerKeys.PORTLET_USER_PREFERENCES +
+						StringPool.UNDERLINE + rootPortletId);
 		}
 
-		return new boolean[] {importCurPortletData, importCurPortletSetup};
+		return new boolean[] {
+			importCurPortletArchivedSetups, importCurPortletData,
+			importCurPortletSetup, importCurPortletUserPreferences};
 	}
 
 	protected String getRootPortletId(long companyId, String portletId)
