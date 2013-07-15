e19c10f114aecbbb63e455c566781a4435f63cbd
ealonso <eudaldo.alonso@liferay.com>
2013-07-15 11:07:57 +0200

    LPS-36894 Autogenerad

--- portal-service/src/com/liferay/portlet/messageboards/service/MBMessageLocalService.java
+++ portal-service/src/com/liferay/portlet/messageboards/service/MBMessageLocalService.java
@@ -326,6 +326,13 @@ public interface MBMessageLocalService extends BaseLocalService,
 	public com.liferay.portlet.messageboards.model.MBMessage addMessage(
 		long userId, java.lang.String userName, long groupId, long categoryId,
 		java.lang.String subject, java.lang.String body,
+		com.liferay.portal.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	public com.liferay.portlet.messageboards.model.MBMessage addMessage(
+		long userId, java.lang.String userName, long groupId, long categoryId,
+		java.lang.String subject, java.lang.String body,
 		java.lang.String format,
 		java.util.List<com.liferay.portal.kernel.util.ObjectValuePair<java.lang.String, java.io.InputStream>> inputStreamOVPs,
 		boolean anonymous, double priority, boolean allowPingbacks,
