f5ade222959694482d135493ea4081c87af917f0
ealonso <eudaldo.alonso@liferay.com>
2013-07-22 13:08:26 +0200

    LPS-34344 Autogenerated

--- portal-service/src/com/liferay/portlet/messageboards/model/MBThreadWrapper.java
+++ portal-service/src/com/liferay/portlet/messageboards/model/MBThreadWrapper.java
@@ -1021,6 +1021,12 @@ public class MBThreadWrapper implements MBThread, ModelWrapper<MBThread> {
 	}
 
 	@Override
+	public long[] getParticipantsIds()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _mbThread.getParticipantsIds();
+	}
+
+	@Override
 	public com.liferay.portlet.messageboards.model.MBCategory getTrashContainer() {
 		return _mbThread.getTrashContainer();
 	}
