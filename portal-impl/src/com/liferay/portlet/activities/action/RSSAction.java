3146fc4d6454649f6bbe438e4bdece6e649107aa
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2013-07-24 10:03:27 +0200

    LPS-37929 Consistency. Obtaining the list of activities in the RSSAction.

--- portal-impl/src/com/liferay/portlet/activities/action/RSSAction.java
+++ portal-impl/src/com/liferay/portlet/activities/action/RSSAction.java
@@ -14,8 +14,18 @@
 
 package com.liferay.portlet.activities.action;
 
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.rss.RSSRenderer;
 import com.liferay.portlet.rss.action.DefaultRSSAction;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.service.SocialActivityLocalServiceUtil;
+
+import java.util.List;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -32,7 +42,39 @@ public class RSSAction extends DefaultRSSAction {
 			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 		throws Exception {
 
-		return new ActivitiesRSSRenderer(resourceRequest, resourceResponse);
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		int max = ParamUtil.getInteger(
+			resourceRequest, "max", SearchContainer.DEFAULT_DELTA);
+
+		Group group = GroupLocalServiceUtil.getGroup(
+			themeDisplay.getScopeGroupId());
+
+		List<SocialActivity> activities = null;
+
+		if (group.isOrganization()) {
+			activities =
+				SocialActivityLocalServiceUtil.getOrganizationActivities(
+					group.getOrganizationId(), 0, max);
+		}
+		else if (group.isRegularSite()) {
+			activities = SocialActivityLocalServiceUtil.getGroupActivities(
+				group.getGroupId(), 0, max);
+		}
+		else if (group.isUser()) {
+			activities = SocialActivityLocalServiceUtil.getUserActivities(
+				group.getClassPK(), 0, max);
+		}
+
+		if (activities != null) {
+			return new ActivitiesRSSRenderer(
+				activities, resourceRequest, resourceResponse);
+		}
+		else {
+			throw new UnsupportedOperationException();
+		}
+
 	}
 
 }
