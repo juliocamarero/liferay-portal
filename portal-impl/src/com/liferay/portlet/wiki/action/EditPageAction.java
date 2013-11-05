43fcaa04ee7f1318be55103e1c2a1b5ba734da35
Roberto Díaz <roberto.diaz@liferay.com>
2013-11-05 12:48:15 +0100

    LPS-41267 Restore should call trash service in wiki portlet to make it more flexible

--- portal-impl/src/com/liferay/portlet/wiki/action/EditPageAction.java
+++ portal-impl/src/com/liferay/portlet/wiki/action/EditPageAction.java
@@ -36,6 +36,9 @@ import com.liferay.portlet.PortletResponseImpl;
 import com.liferay.portlet.PortletURLImpl;
 import com.liferay.portlet.asset.AssetCategoryException;
 import com.liferay.portlet.asset.AssetTagException;
+import com.liferay.portlet.trash.model.TrashEntry;
+import com.liferay.portlet.trash.service.TrashEntryLocalServiceUtil;
+import com.liferay.portlet.trash.service.TrashEntryServiceUtil;
 import com.liferay.portlet.trash.util.TrashUtil;
 import com.liferay.portlet.wiki.DuplicatePageException;
 import com.liferay.portlet.wiki.NoSuchNodeException;
@@ -343,11 +346,30 @@ public class EditPageAction extends PortletAction {
 	}
 
 	protected void restorePage(ActionRequest actionRequest) throws Exception {
+		long nodeId = ParamUtil.getLong(actionRequest, "nodeId");
 		long[] restoreEntryIds = StringUtil.split(
 			ParamUtil.getString(actionRequest, "restoreEntryIds"), 0L);
+		String title = ParamUtil.getString(actionRequest, "title");
+
+		long overridePageResourcePrimKey = 0L;
+
+		if ((nodeId != 0) && Validator.isNotNull(title) &&
+			title.equals(WikiPageConstants.FRONT_PAGE)) {
+
+			WikiPage overridePage = WikiPageServiceUtil.fetchPage(
+				nodeId, title, 0);
+
+			if (overridePage != null) {
+				overridePageResourcePrimKey = overridePage.getResourcePrimKey();
+			}
+		}
 
 		for (long restoreEntryId : restoreEntryIds) {
-			WikiPageServiceUtil.restorePageFromTrash(restoreEntryId);
+			TrashEntry trashEntry = TrashEntryLocalServiceUtil.getEntry(
+				WikiPage.class.getName(), restoreEntryId);
+
+			TrashEntryServiceUtil.restoreEntry(
+				trashEntry.getEntryId(), overridePageResourcePrimKey, null);
 		}
 	}
 
