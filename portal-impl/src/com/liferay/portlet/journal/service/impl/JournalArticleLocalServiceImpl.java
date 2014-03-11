8ad8f5431df7d8f17aaf2650b6ca7a8d7af99594
Julio Camarero <julio.camarero@liferay.com>
2014-03-11 17:33:38 +0100

    LPS-33427 SF - Use a double className to differentiate from toher DDMStructures with className 0

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -4424,8 +4424,16 @@ public class JournalArticleLocalServiceImpl
 			long groupId, long userId, long ddmStructureId)
 		throws PortalException, SystemException {
 
+		String className = DDMStructure.class.getName();
+		long classPK = ddmStructureId;
+
+		if (ddmStructureId == 0) {
+			className += JournalArticle.class.getName();
+			classPK = groupId;
+		}
+
 		subscriptionLocalService.addSubscription(
-			userId, groupId, DDMStructure.class.getName(), ddmStructureId);
+			userId, groupId, className, classPK);
 	}
 
 	/**
@@ -4444,8 +4452,15 @@ public class JournalArticleLocalServiceImpl
 		long groupId, long userId, long ddmStructureId) throws PortalException,
 		SystemException {
 
-		subscriptionLocalService.deleteSubscription(
-			userId, DDMStructure.class.getName(), ddmStructureId);
+		String className = DDMStructure.class.getName();
+		long classPK = ddmStructureId;
+
+		if (ddmStructureId == 0) {
+			className += JournalArticle.class.getName();
+			classPK = groupId;
+		}
+
+		subscriptionLocalService.deleteSubscription(userId, className, classPK);
 	}
 
 	/**
