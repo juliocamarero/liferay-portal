1f90c9e03b3c277106a849575634ff7d96e4d67e
Minhchau Dang <minhchau.dang@liferay.com>
2014-04-04 07:41:20 -0400

    LPS-45491 Simplify

--- portal-impl/src/com/liferay/portal/service/impl/UserGroupRoleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserGroupRoleLocalServiceImpl.java
@@ -41,9 +41,7 @@ public class UserGroupRoleLocalServiceImpl
 
 		List<UserGroupRole> userGroupRoles = new ArrayList<UserGroupRole>();
 
-		if (!userLocalService.hasGroupUser(groupId, userId)) {
- 			userLocalService.addGroupUser(groupId, userId);
-		}
+		userLocalService.addGroupUser(groupId, userId);
 
 		for (long roleId : roleIds) {
 			UserGroupRole userGroupRole = addUserGroupRole(
@@ -64,11 +62,9 @@ public class UserGroupRoleLocalServiceImpl
 
 		List<UserGroupRole> userGroupRoles = new ArrayList<UserGroupRole>();
 
-		for (long userId : userIds) {
-			if (!userLocalService.hasGroupUser(groupId, userId)) {
-				userLocalService.addGroupUser(groupId, userId);
-			}
+		userLocalService.addGroupUsers(groupId, userId);
 
+		for (long userId : userIds) {
 			UserGroupRole userGroupRole = addUserGroupRole(
 				userId, groupId, roleId);
 
