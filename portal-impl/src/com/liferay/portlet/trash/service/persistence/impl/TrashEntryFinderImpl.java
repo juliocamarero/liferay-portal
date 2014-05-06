5aa38a37fd23b45243495bc9a700bcd0b6c16860
Sampsa Sohlman <sampsa.sohlman@liferay.com>
2014-05-06 13:27:18 +0300

    LPS-44048 finder method to find all expiring trash entries per company id

--- portal-impl/src/com/liferay/portlet/trash/service/persistence/impl/TrashEntryFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/trash/service/persistence/impl/TrashEntryFinderImpl.java
@@ -20,6 +20,8 @@ import com.liferay.portal.kernel.dao.orm.SQLQuery;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.model.impl.GroupImpl;
 import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
 import com.liferay.portlet.trash.model.TrashEntry;
@@ -35,8 +37,56 @@ import java.util.List;
 public class TrashEntryFinderImpl extends BasePersistenceImpl<TrashEntry>
 		implements TrashEntryFinder {
 
+	public static final String FIND_CLEANABLE_ENTRIES =
+		TrashEntryFinder.class.getName() + ".findCleanableEntries";
+
 	public static final String FIND_TRASHENTRY_WITH_GROUP =
 		TrashEntryFinder.class.getName() + ".findTrashEntryWithGroup";
+
+	public List<TrashEntry> findCleanableEntries(
+			long trashEntryId, long companyId, int start, int end)
+		throws PortalException, SystemException {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(FIND_CLEANABLE_ENTRIES);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addEntity("te", TrashEntryImpl.class);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			int currentTimeInMinutes =
+				(int) (System.currentTimeMillis() / 60000L);
+
+			int companyTrashEntriesMaxAge = PrefsPropsUtil.getInteger(
+				companyId, PropsKeys.TRASH_ENTRIES_MAX_AGE);
+
+			qPos.add(currentTimeInMinutes);
+			qPos.add(companyTrashEntriesMaxAge);
+			qPos.add(currentTimeInMinutes);
+
+			qPos.add(currentTimeInMinutes);
+			qPos.add(companyTrashEntriesMaxAge);
+			qPos.add(currentTimeInMinutes);
+
+			qPos.add(companyId);
+			qPos.add(trashEntryId);
+
+			return (List<TrashEntry>)QueryUtil.list(
+				q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
 
 	public List<Object[]> findTrashEntryWithGroup(
 			long trashEntryId, int start, int end)
