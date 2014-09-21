b16f3d6b06da409e3812107038e5e6ff24384e07
John Zhao <john.zhao@liferay.com>
2014-09-21 19:22:19 +0800

    LPS-50250 Web content should not be displayed on a WCD whose scope does not equal that of web content.

--- portal-impl/src/com/liferay/portlet/journalcontent/action/ViewAction.java
+++ portal-impl/src/com/liferay/portlet/journalcontent/action/ViewAction.java
@@ -72,7 +72,7 @@ public class ViewAction extends WebContentAction {
 		JournalArticle article = null;
 		JournalArticleDisplay articleDisplay = null;
 
-		if ((articleGroupId > 0) && Validator.isNotNull(articleId)) {
+		if ((articleGroupId > 0) && Validator.equals(articleGroupId, themeDisplay.getScopeGroupId()) && Validator.isNotNull(articleId)) {
 			String viewMode = ParamUtil.getString(renderRequest, "viewMode");
 			String languageId = LanguageUtil.getLanguageId(renderRequest);
 			int page = ParamUtil.getInteger(renderRequest, "page", 1);
