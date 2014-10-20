69e301df59a6e63d14743487ab9bef53576eebe8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-10-20 12:51:19 +0200

    Moves code from ExportArticleAction to JournalContentPortlet java

--- modules/apps/journal/journal-content-web/src/com/liferay/journal/content/web/portlet/JournalContentPortlet.java
+++ modules/apps/journal/journal-content-web/src/com/liferay/journal/content/web/portlet/JournalContentPortlet.java
@@ -15,28 +15,49 @@
 package com.liferay.journal.content.web.portlet;
 
 import com.liferay.journal.content.web.portlet.upgrade.JournalContentUpgrade;
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.PortletRequestModel;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.DocumentConversionUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PrefsParamUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.documentlibrary.util.DLUtil;
 import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.model.JournalArticleDisplay;
 import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.portlet.journalcontent.util.JournalContentUtil;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -161,9 +182,128 @@ public class JournalContentPortlet extends MVCPortlet {
 		super.doView(renderRequest, renderResponse);
 	}
 
+	public void exportAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			long groupId = ParamUtil.getLong(actionRequest, "groupId");
+			String articleId = ParamUtil.getString(actionRequest, "articleId");
+
+			String targetExtension = ParamUtil.getString(
+				actionRequest, "targetExtension");
+
+			PortletPreferences portletPreferences =
+				actionRequest.getPreferences();
+
+			String[] allowedExtensions = StringUtil.split(
+				portletPreferences.getValue("extensions", null));
+
+			String languageId = LanguageUtil.getLanguageId(actionRequest);
+			PortletRequestModel portletRequestModel = new PortletRequestModel(
+				actionRequest, actionResponse);
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+			HttpServletRequest request = PortalUtil.getHttpServletRequest(
+				actionRequest);
+			HttpServletResponse response = PortalUtil.getHttpServletResponse(
+				actionResponse);
+
+			getFile(
+				groupId, articleId, targetExtension, allowedExtensions,
+				languageId, portletRequestModel, themeDisplay, request,
+				response);
+		}
+		catch (Exception e) {
+			PortalUtil.sendError(e, actionRequest, actionResponse);
+		}
+	}
+
+	protected void getFile(
+			long groupId, String articleId, String targetExtension,
+			String[] allowedExtensions, String languageId,
+			PortletRequestModel portletRequestModel, ThemeDisplay themeDisplay,
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		try {
+			JournalArticleDisplay articleDisplay =
+				JournalContentUtil.getDisplay(
+					groupId, articleId, null, "export", languageId, 1,
+					portletRequestModel, themeDisplay);
+
+			int pages = articleDisplay.getNumberOfPages();
+
+			StringBundler sb = new StringBundler(pages + 12);
+
+			sb.append("<html>");
+
+			sb.append("<head>");
+			sb.append("<meta content=\"");
+			sb.append(ContentTypes.TEXT_HTML_UTF8);
+			sb.append("\" http-equiv=\"content-type\" />");
+			sb.append("<base href=\"");
+			sb.append(themeDisplay.getPortalURL());
+			sb.append("\" />");
+			sb.append("</head>");
+
+			sb.append("<body>");
+
+			sb.append(articleDisplay.getContent());
+
+			for (int i = 2; i <= pages; i++) {
+				articleDisplay = JournalContentUtil.getDisplay(
+					groupId, articleId, "export", languageId, i, themeDisplay);
+
+				sb.append(articleDisplay.getContent());
+			}
+
+			sb.append("</body>");
+			sb.append("</html>");
+
+			InputStream is = new UnsyncByteArrayInputStream(
+				sb.toString().getBytes(StringPool.UTF8));
+
+			String title = articleDisplay.getTitle();
+			String sourceExtension = "html";
+
+			String fileName = title.concat(StringPool.PERIOD).concat(
+				sourceExtension);
+
+			if (Validator.isNotNull(targetExtension) &&
+				ArrayUtil.contains(allowedExtensions, targetExtension)) {
+
+				String id = DLUtil.getTempFileId(
+					articleDisplay.getId(),
+					String.valueOf(articleDisplay.getVersion()), languageId);
+
+				File convertedFile = DocumentConversionUtil.convert(
+					id, is, sourceExtension, targetExtension);
+
+				if (convertedFile != null) {
+					fileName = title.concat(StringPool.PERIOD).concat(
+						targetExtension);
+
+					is = new FileInputStream(convertedFile);
+				}
+			}
+
+			String contentType = MimeTypesUtil.getContentType(fileName);
+
+			ServletResponseUtil.sendFile(
+				request, response, fileName, is, contentType);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+	}
+
 	@Reference(unbind = "-")
 	protected void setInvitationUpgrade(
 		JournalContentUpgrade journalContentUpgrade) {
 	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		JournalContentPortlet.class);
 
 }
