5553ed8e31b7d97847d3b9efac840ff7feafb834
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-01 16:33:36 +0100

    LPS-51880 Uses the new method

--- portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
@@ -411,14 +411,7 @@ public class JournalArticleIndexer extends BaseIndexer {
 		document.addKeyword("ddmStructureKey", article.getDDMStructureKey());
 		document.addKeyword("ddmTemplateKey", article.getDDMTemplateKey());
 		document.addDate("displayDate", article.getDisplayDate());
-
-		boolean head = true;
-
-		if (PropsValues.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS) {
-			head = isHead(article);
-		}
-
-		document.addKeyword("head", head);
+		document.addKeyword("head", isHead(article));
 
 		addDDMStructureAttributes(document, article);
 
@@ -563,15 +556,11 @@ public class JournalArticleIndexer extends BaseIndexer {
 		}
 
 		List<JournalArticle> articles =
-			JournalArticleLocalServiceUtil.getStructureArticles(
+			JournalArticleLocalServiceUtil.getArticlesByDDMStructureKey(
 				ddmStructureKeys);
 
 		for (JournalArticle article : articles) {
-			if (PropsValues.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS ||
-				isHead(article)) {
-
-				doReindex(article, false);
-			}
+			doReindex(article, false);
 		}
 	}
 
@@ -694,6 +683,10 @@ public class JournalArticleIndexer extends BaseIndexer {
 	}
 
 	protected boolean isHead(JournalArticle article) {
+		if (!PropsValues.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS) {
+			return true;
+		}
+
 		JournalArticle latestArticle =
 			JournalArticleLocalServiceUtil.fetchLatestArticle(
 				article.getResourcePrimKey(),
