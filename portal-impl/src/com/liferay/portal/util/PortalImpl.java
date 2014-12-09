ce285f545b077cfc5d451baa7e4ff0b15b678d74
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-09 12:30:14 +0100

    Removes no needed properties

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1307,9 +1307,7 @@ public class PortalImpl implements Portal {
 			groups.add(0, organization.getGroup());
 		}
 
-		if (PropsValues.LAYOUT_USER_PRIVATE_LAYOUTS_ENABLED ||
-			PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_ENABLED) {
-
+		if (PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_ENABLED) {
 			groups.add(
 				0, GroupLocalServiceUtil.getUserGroup(companyId, userId));
 		}
