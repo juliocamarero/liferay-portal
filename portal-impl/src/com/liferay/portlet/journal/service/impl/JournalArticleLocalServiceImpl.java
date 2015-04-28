233d532694f90eb24d242f6d9ea907c03e01b25f
Preston-Crary <preston.crary@liferay.com>
2015-04-27 18:40:59 -0700

    LPS-55246 SF: Remove places where we set create and modified dates before immediately calling the persistence as these are now redundant, this has the benifit of always using the service context when it applies. I left some like the ones in SCProductVersionLocalServiceImpl because that is all we change with the ProductEntry and it might be weird to call update after not changing anything.  I also had to leave the ones for Layout, LayoutSet, LayoutPrototype, and LayoutSetPrototype because modifiedDate has a different meaning for template merging.

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -367,8 +367,6 @@ public class JournalArticleLocalServiceImpl
 		article.setCompanyId(user.getCompanyId());
 		article.setUserId(user.getUserId());
 		article.setUserName(user.getFullName());
-		article.setCreateDate(serviceContext.getCreateDate(now));
-		article.setModifiedDate(serviceContext.getModifiedDate(now));
 		article.setFolderId(folderId);
 		article.setClassNameId(classNameId);
 		article.setClassPK(classPK);
@@ -760,7 +758,6 @@ public class JournalArticleLocalServiceImpl
 		User user = userPersistence.findByPrimaryKey(userId);
 		oldArticleId = StringUtil.toUpperCase(oldArticleId.trim());
 		newArticleId = StringUtil.toUpperCase(newArticleId.trim());
-		Date now = new Date();
 
 		JournalArticle oldArticle = journalArticlePersistence.findByG_A_V(
 			groupId, oldArticleId, version);
@@ -799,8 +796,6 @@ public class JournalArticleLocalServiceImpl
 		newArticle.setCompanyId(user.getCompanyId());
 		newArticle.setUserId(user.getUserId());
 		newArticle.setUserName(user.getFullName());
-		newArticle.setCreateDate(now);
-		newArticle.setModifiedDate(now);
 		newArticle.setFolderId(oldArticle.getFolderId());
 		newArticle.setTreePath(oldArticle.getTreePath());
 		newArticle.setArticleId(newArticleId);
@@ -3430,7 +3425,6 @@ public class JournalArticleLocalServiceImpl
 			}
 
 			article.setFolderId(newFolderId);
-			article.setModifiedDate(new Date());
 			article.setTreePath(article.buildTreePath());
 
 			journalArticlePersistence.update(article);
@@ -3524,8 +3518,6 @@ public class JournalArticleLocalServiceImpl
 		throws PortalException {
 
 		// Article
-
-		article.setModifiedDate(new Date());
 
 		int oldStatus = article.getStatus();
 
@@ -3713,8 +3705,6 @@ public class JournalArticleLocalServiceImpl
 			article.setContent(content);
 		}
 
-		article.setModifiedDate(new Date());
-
 		journalArticlePersistence.update(article);
 
 		return article;
@@ -3754,7 +3744,6 @@ public class JournalArticleLocalServiceImpl
 		}
 
 		article.setArticleId(trashArticleId);
-		article.setModifiedDate(new Date());
 
 		journalArticlePersistence.update(article);
 
@@ -5222,7 +5211,6 @@ public class JournalArticleLocalServiceImpl
 			user, groupId, articleId, article.getVersion(), addNewVersion,
 			content, images);
 
-		article.setModifiedDate(serviceContext.getModifiedDate(now));
 		article.setFolderId(folderId);
 		article.setTreePath(article.buildTreePath());
 		article.setTitleMap(titleMap, locale);
@@ -5475,7 +5463,6 @@ public class JournalArticleLocalServiceImpl
 			article.setUserId(oldArticle.getUserId());
 			article.setUserName(user.getFullName());
 			article.setCreateDate(oldArticle.getCreateDate());
-			article.setModifiedDate(new Date());
 			article.setClassNameId(oldArticle.getClassNameId());
 			article.setClassPK(oldArticle.getClassPK());
 			article.setArticleId(articleId);
