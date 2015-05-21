4d5c2ae3e5f047b126e3caaee3f5992634263ee6
arbetts <andrew.betts@liferay.com>
2015-05-21 12:19:38 -0700

    LPS-55098 ant build service portlet asset

--- portal-service/src/com/liferay/portlet/asset/service/AssetEntryLocalServiceWrapper.java
+++ portal-service/src/com/liferay/portlet/asset/service/AssetEntryLocalServiceWrapper.java
@@ -495,13 +495,13 @@ public class AssetEntryLocalServiceWrapper implements AssetEntryLocalService,
 		long[] groupIds, long[] classNameIds, java.lang.String keywords,
 		java.lang.String userName, java.lang.String title,
 		java.lang.String description, boolean advancedSearch,
-		boolean andOperator, int start, int end, java.lang.String orderByCol1,
-		java.lang.String orderByCol2, java.lang.String orderByType1,
-		java.lang.String orderByType2) {
+		boolean andOperator, boolean includeNonListable, int start, int end,
+		java.lang.String orderByCol1, java.lang.String orderByCol2,
+		java.lang.String orderByType1, java.lang.String orderByType2) {
 		return _assetEntryLocalService.getEntries(groupIds, classNameIds,
 			keywords, userName, title, description, advancedSearch,
-			andOperator, start, end, orderByCol1, orderByCol2, orderByType1,
-			orderByType2);
+			andOperator, includeNonListable, start, end, orderByCol1,
+			orderByCol2, orderByType1, orderByType2);
 	}
 
 	@Override
@@ -514,9 +514,10 @@ public class AssetEntryLocalServiceWrapper implements AssetEntryLocalService,
 	public int getEntriesCount(long[] groupIds, long[] classNameIds,
 		java.lang.String keywords, java.lang.String userName,
 		java.lang.String title, java.lang.String description,
-		boolean advancedSearch, boolean andOperator) {
+		boolean advancedSearch, boolean andOperator, boolean includeNonListable) {
 		return _assetEntryLocalService.getEntriesCount(groupIds, classNameIds,
-			keywords, userName, title, description, advancedSearch, andOperator);
+			keywords, userName, title, description, advancedSearch,
+			andOperator, includeNonListable);
 	}
 
 	@Override
