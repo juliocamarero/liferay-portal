b08e765cb51fc1bd91314bcde5e057ff337f0f2b
Tamas Molnar <tamas.molnar@liferay.com>
2015-06-16 16:57:58 +0200

    LPS-56408 Implement the getEntry() method for the AssetRenderers

--- modules/apps/dynamic-data-lists/dynamic-data-lists-web/src/com/liferay/dynamic/data/lists/web/asset/DDLRecordAssetRenderer.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-web/src/com/liferay/dynamic/data/lists/web/asset/DDLRecordAssetRenderer.java
@@ -93,6 +93,11 @@ public class DDLRecordAssetRenderer extends BaseJSPAssetRenderer {
 	}
 
 	@Override
+	public Object getEntry() {
+		return _record;
+	}
+
+	@Override
 	public long getGroupId() {
 		return _record.getGroupId();
 	}
--- modules/apps/journal/journal-web/src/com/liferay/journal/web/asset/JournalFolderAssetRenderer.java
+++ modules/apps/journal/journal-web/src/com/liferay/journal/web/asset/JournalFolderAssetRenderer.java
@@ -74,6 +74,11 @@ public class JournalFolderAssetRenderer
 	}
 
 	@Override
+	public Object getEntry() {
+		return _folder;
+	}
+
+	@Override
 	public long getGroupId() {
 		return _folder.getGroupId();
 	}
