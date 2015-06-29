ce5d705a66b2d5313ab4dd3bb24d528a211c6b65
Juergen Kappler <jurgen.kappler@liferay.com>
2015-06-29 17:38:38 +0200

    LPS-56702 Use BaseJSPSettingsConfigurationAction instead of the DefaultConfigurationAction to reflect new settings changes

--- modules/apps/journal/journal-web/src/com/liferay/journal/web/portlet/action/JournalConfigurationAction.java
+++ modules/apps/journal/journal-web/src/com/liferay/journal/web/portlet/action/JournalConfigurationAction.java
@@ -14,18 +14,13 @@
 
 package com.liferay.journal.web.portlet.action;
 
-import com.liferay.journal.configuration.JournalServiceConfigurationUtil;
-import com.liferay.journal.configuration.JournalServiceConfigurationValues;
 import com.liferay.journal.web.constants.JournalPortletKeys;
+import com.liferay.portal.kernel.portlet.BaseJSPSettingsConfigurationAction;
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
-import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
-import com.liferay.portal.kernel.util.LocaleUtil;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
-import javax.portlet.PortletPreferences;
-import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 
 import javax.servlet.ServletContext;
@@ -42,113 +37,12 @@ import org.osgi.service.component.annotations.Reference;
 	property = {"javax.portlet.name=" + JournalPortletKeys.JOURNAL},
 	service = ConfigurationAction.class
 )
-public class JournalConfigurationAction extends DefaultConfigurationAction {
+public class JournalConfigurationAction
+	extends BaseJSPSettingsConfigurationAction {
 
 	@Override
 	public String getJspPath(RenderRequest renderRequest) {
 		return "/configuration.jsp";
-	}
-
-	@Override
-	public void postProcess(
-		long companyId, PortletRequest portletRequest,
-		PortletPreferences portletPreferences) {
-
-		String languageId = LocaleUtil.toLanguageId(
-			LocaleUtil.getSiteDefault());
-
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleAddedBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.EMAIL_ARTICLE_ADDED_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleAddedSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.EMAIL_ARTICLE_ADDED_SUBJECT));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleApprovalDeniedBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_APPROVAL_DENIED_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleApprovalDeniedSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleApprovalGrantedBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_APPROVAL_GRANTED_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleApprovalGrantedSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleApprovalRequestedBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleApprovalRequestedSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleMovedFromFolderBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleMovedFromFolderSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleMovedToFolderBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleMovedToFolderSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleReviewBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.EMAIL_ARTICLE_REVIEW_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleReviewSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_REVIEW_SUBJECT));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleUpdatedBody_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.EMAIL_ARTICLE_UPDATED_BODY));
-		removeDefaultValue(
-			portletRequest, portletPreferences,
-			"emailArticleUpdatedSubject_" + languageId,
-			JournalServiceConfigurationUtil.getContent(
-				JournalServiceConfigurationValues.
-					EMAIL_ARTICLE_UPDATED_SUBJECT));
 	}
 
 	@Override
