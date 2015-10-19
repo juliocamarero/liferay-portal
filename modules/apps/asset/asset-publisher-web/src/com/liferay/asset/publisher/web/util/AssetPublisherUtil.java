731ca71d98674967bf6e382c96f0fb99ceed67e6
David Zhang <david.zhang@liferay.com>
2015-10-19 17:12:31 +0800

    LPS-58591 Getting double add entry button on Asset publisher portlet

--- modules/apps/asset/asset-publisher-web/src/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java
+++ modules/apps/asset/asset-publisher-web/src/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java
@@ -1048,7 +1048,7 @@ public class AssetPublisherUtil {
 		String[] scopeIds = portletPreferences.getValues(
 			"scopeIds", new String[] {SCOPE_ID_GROUP_PREFIX + scopeGroupId});
 
-		List<Long> groupIds = new ArrayList<>();
+		Set<Long> groupIds = new HashSet<>();
 
 		for (String scopeId : scopeIds) {
 			try {
