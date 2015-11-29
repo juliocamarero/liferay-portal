bb82c4768891d2e36ad23014051fabfc2f4478e1
Chema Balsas <jose.balsas@liferay.com>
2015-11-29 19:04:40 +0100

    LPS-59825 Extract Google Map module

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/modules.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/modules.js
@@ -484,12 +484,6 @@
 							'liferay-portlet-base'
 						]
 					},
-					'liferay-map-google': {
-						path: 'map_google.js',
-						requires: [
-							'liferay-map-base'
-						]
-					},
 					'liferay-map-openstreet': {
 						path: 'map_openstreet.js',
 						requires: [
