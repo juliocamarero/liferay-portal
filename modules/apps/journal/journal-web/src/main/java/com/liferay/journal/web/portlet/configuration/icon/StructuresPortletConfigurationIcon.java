0edb5f8ee6d707c800f590ca0b75840d83902e3b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-01-27 13:36:09 +0100

    LPS-62586 Don't open structures on a pop-up

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/portlet/configuration/icon/StructuresPortletConfigurationIcon.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/portlet/configuration/icon/StructuresPortletConfigurationIcon.java
@@ -14,10 +14,20 @@
 
 package com.liferay.journal.web.portlet.configuration.icon;
 
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.webdav.WebDAVUtil;
+import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.User;
+import com.liferay.portal.service.PortletLocalServiceUtil;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portlet.PortletURLFactoryUtil;
 
 import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
 
 /**
  * @author Eudaldo Alonso
@@ -36,7 +46,62 @@ public class StructuresPortletConfigurationIcon
 
 	@Override
 	public String getURL() {
-		return "javascript:;";
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		Portlet portlet = PortletLocalServiceUtil.getPortletById(
+			portletDisplay.getId());
+
+		PortletURL portletURL = PortletURLFactoryUtil.create(
+			portletRequest,
+			PortletProviderUtil.getPortletId(
+				DDMStructure.class.getName(), PortletProvider.Action.VIEW),
+			themeDisplay.getPlid(), PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcPath", "/view.jsp");
+		portletURL.setParameter(
+			"groupId", String.valueOf(themeDisplay.getScopeGroupId()));
+		portletURL.setParameter("redirect", themeDisplay.getURLCurrent());
+		portletURL.setParameter(
+			"refererPortletName", JournalPortletKeys.JOURNAL);
+		portletURL.setParameter(
+			"refererWebDAVToken", WebDAVUtil.getStorageToken(portlet));
+		portletURL.setParameter("showAncestorScopes", Boolean.TRUE.toString());
+		portletURL.setParameter("showManageTemplates", Boolean.TRUE.toString());
+
+		/*
+		ddmURL.setDoAsGroupId(
+			config.doAsGroupId || themeDisplay.getScopeGroupId());
+
+		ddmURL.setParameter("classNameId", config.classNameId);
+		ddmURL.setParameter("classPK", config.classPK);
+		ddmURL.setParameter("resourceClassNameId", config.resourceClassNameId);
+		ddmURL.setParameter("eventName", config.eventName);
+		ddmURL.setParameter("mode", config.mode);
+		ddmURL.setParameter(
+			"portletResourceNamespace", config.portletResourceNamespace);
+
+		ddmURL.setParameter("scopeTitle", config.title);
+
+		if ("showBackURL" in config) {
+			ddmURL.setParameter("showBackURL", config.showBackURL);
+		}
+
+		if ("showHeader" in config) {
+			ddmURL.setParameter("showHeader", config.showHeader);
+		}
+
+		if ("showToolbar" in config) {
+			ddmURL.setParameter("showToolbar", config.showToolbar);
+		}
+
+		ddmURL.setParameter(
+			"structureAvailableFields", config.structureAvailableFields);
+		ddmURL.setParameter("templateId", config.templateId);
+
+		ddmURL.setPortletId(Liferay.PortletKeys.DYNAMIC_DATA_MAPPING);
+		*/
+
+		return portletURL.toString();
 	}
 
 	@Override
