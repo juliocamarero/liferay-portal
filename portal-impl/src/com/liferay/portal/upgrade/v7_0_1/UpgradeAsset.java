bb803dc66c47f86cba3ef52f18ef951d36269cc2
Julio Camarero <julio.camarero@liferay.com>
2016-04-22 10:02:58 +0200

    LPS-65107 simplify query

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_1/UpgradeAsset.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_1/UpgradeAsset.java
@@ -5,6 +5,7 @@ import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.StringBundler;
 
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,35 +13,14 @@ public class UpgradeAsset extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		_addAssetPublishDate();
+		addAssetEntriesPublishDate();
 	}
 
-	private void _addAssetPublishDate() throws SQLException {
+	protected void addAssetEntriesPublishDate() throws SQLException, IOException {
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			StringBundler sb = new StringBundler();
-
-			sb.append("select AssetEntry.entryId, AssetEntry.createDate ");
-			sb.append("from AssetEntry where publishDate is null ");
-
-			try (PreparedStatement ps1 = connection.prepareStatement(
-					sb.toString())) {
-
-				try (PreparedStatement ps2 =
-						AutoBatchPreparedStatementUtil.concurrentAutoBatch(
-							connection,
-							"update AssetEntry set publishDate = ? where " +
-								"entryId = ?");
-					ResultSet rs = ps1.executeQuery()) {
-
-					while (rs.next()) {
-						ps2.setTimestamp(1, rs.getTimestamp(2));
-						ps2.setLong(2, rs.getLong(1));
-						ps2.addBatch();
-					}
-
-					ps2.executeBatch();
-				}
-			}
+			runSQL(
+				"update AssetEntry set publishDate = createDate where " +
+				"publishDate is null");
 		}
 	}
 
