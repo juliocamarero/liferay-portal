b6777bee298f08d7d013863e0ab1e3f4417e7dac
Jorge Díaz <jorge.diaz@liferay.com>
2016-06-13 17:54:52 +0200

    LPS-66570 Add staging logic to isActive()

--- portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
@@ -809,6 +809,21 @@ public class GroupImpl extends GroupBaseImpl {
 		}
 	}
 
+	@Override
+	public boolean isActive() {
+		if (!isStagingGroup()) {
+			return super.isActive();
+		}
+
+		Group liveGroup = getLiveGroup();
+
+		if (liveGroup == null) {
+			return false;
+		}
+
+		return liveGroup.isActive();
+	}
+
 	/**
 	 * @deprecated As of 7.0.0, replaced by {@link #hasAncestor}
 	 */
