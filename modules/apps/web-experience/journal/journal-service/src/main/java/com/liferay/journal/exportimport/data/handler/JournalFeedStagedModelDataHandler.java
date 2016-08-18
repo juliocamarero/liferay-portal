cf50d3756b83b8093f88224ef29530096edeba07
Pavel Savinov <pavel.savinov@liferay.com>
2016-08-18 09:35:27 +0200

    LPS-58672 fix integration tests

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/exportimport/data/handler/JournalFeedStagedModelDataHandler.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/exportimport/data/handler/JournalFeedStagedModelDataHandler.java
@@ -335,7 +335,6 @@ public class JournalFeedStagedModelDataHandler
 	 * @deprecated As of 7.0.0
 	 */
 	@Deprecated
-	@Reference(unbind = "-")
 	protected void setJournalFeedExportImportContentProcessor(
 		JournalFeedExportImportContentProcessor
 			journalFeedExportImportContentProcessor) {
@@ -359,9 +358,8 @@ public class JournalFeedStagedModelDataHandler
 		_exportImportContentProcessorController;
 
 	private JournalCreationStrategy _journalCreationStrategy;
-	private JournalFeedExportImportContentProcessor
+	private final JournalFeedExportImportContentProcessor
 		_journalFeedExportImportContentProcessor;
-
 	private JournalFeedLocalService _journalFeedLocalService;
 
 }
