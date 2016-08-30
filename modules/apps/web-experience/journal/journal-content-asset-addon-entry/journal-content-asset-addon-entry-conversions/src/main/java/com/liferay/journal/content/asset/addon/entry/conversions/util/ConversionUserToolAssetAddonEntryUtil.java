750aa687a6af3694199723b1396182beed85e539
Pavel Savinov <pavel.savinov@liferay.com>
2016-08-30 12:27:17 +0200

    LPS-67407 add conversion tool util to get extension by addon entry key

---
+++ modules/apps/web-experience/journal/journal-content-asset-addon-entry/journal-content-asset-addon-entry-conversions/src/main/java/com/liferay/journal/content/asset/addon/entry/conversions/util/ConversionUserToolAssetAddonEntryUtil.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.asset.addon.entry.conversions.util;
+
+import com.liferay.journal.content.asset.addon.entry.common.UserToolAssetAddonEntry;
+import com.liferay.journal.content.asset.addon.entry.common.UserToolAssetAddonEntryTracker;
+import com.liferay.journal.content.asset.addon.entry.conversions.internal.BaseConvertionUserToolAssetAddonEntry;
+
+/**
+ * @author Pavel Savinov
+ */
+public class ConversionUserToolAssetAddonEntryUtil {
+
+	public static String getExtension(String key) {
+		UserToolAssetAddonEntry userToolAssetAddonEntry =
+			UserToolAssetAddonEntryTracker.getUserToolAssetAddonEntry(key);
+
+		if (userToolAssetAddonEntry == null) {
+			return null;
+		}
+
+		if (!(userToolAssetAddonEntry instanceof
+				BaseConvertionUserToolAssetAddonEntry)) {
+
+			return null;
+		}
+
+		BaseConvertionUserToolAssetAddonEntry
+			convertionUserToolAssetAddonEntry =
+				(BaseConvertionUserToolAssetAddonEntry)userToolAssetAddonEntry;
+
+		return convertionUserToolAssetAddonEntry.getExtension();
+	}
+
+}
