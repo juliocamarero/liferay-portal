bc0e983f7cb12e3191218e274e6031cd38f72e12
David Arques <davidarchena@gmail.com>
2018-08-29 15:01:57 +0200

    LPS-83359 Add FilterParser entity to API

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-api/src/main/java/com/liferay/structured/content/apio/architect/filter/FilterParser.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.architect.filter;
+
+import org.apache.olingo.server.api.uri.queryoption.expression.Expression;
+
+/**
+ * <code>FilterLexer</code> transforms a sequence of characters in an manageable
+ * OData expression {@link Expression}.
+ *
+ * @author David Arques
+ * @review
+ */
+public interface FilterParser {
+
+	/**
+	 * Returns a {@link Expression} obtained from a String.
+	 *
+	 * @param  filter - the filter
+	 * @return The OData Expression
+	 * @review
+	 */
+	public Expression parse(String filter);
+
+}
